6005e2be9b18db66cc862e3145e41491d26d9086
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-09 15:10:17 +0200

    Add readfile functionality

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const fs = require('node:fs')
+const url = "./test.txt"
 
-const PORT = 3000
+const port = process.env.port || 3000
+
+
+fs.readFile(url, (err, html) => {
+
+  if(err) {
+    throw new Error('no such page')
+  }
+
 http.createServer((req, res) => {
   res.writeHead(200, {"content-type": "text/plain"})
-  res.write('Hello World')
+  res.write(html)
   res.end()
 
-}).listen(PORT)
+}).listen(port)
+})
